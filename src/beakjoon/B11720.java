987de22b3b063aeb2120b1205d12954cbfb25862
kymin <park48664866@gmail.com>
2024-12-25 13:02:10 +0900

    feat: 배열 기본 예제 풀이
    배열 기본 문제 풀이:
    - 1546 평균 구하기 풀이
    - 11720 숫자의 합 구하기 풀이

---
+++ src/beakjoon/B11720.java
@@ -0,0 +1,25 @@
+package beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// https://www.acmicpc.net/problem/11720
+public class B11720 {
+    public static void main(String[] args) throws IOException {
+        // scanner 대신 bufferedReader를 사용하면 메오리와 실행시간 단축(데이터가 버퍼를 거쳐서 전달되므로 효흉성 상승)
+        // bufferedReader는 엔터만 경계로 인식하므로 readLine을 사용
+        // 읽은 데이터는 무조건 String이므로 형변환 필요
+        // readLine을 할 때마다 try-catch로 예외처리 도는 throws로 예외를 명시
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bf.readLine());
+        String[] inputNums = bf.readLine().split("");
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += Integer.parseInt(inputNums[i]);
+        }
+        System.out.println(sum);
+    }
+}
---
+++ src/beakjoon/B1546.java
@@ -0,0 +1,34 @@
+package beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// https://www.acmicpc.net/problem/1546
+public class B1546 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bf.readLine());
+        int[] scores = new int[n];
+
+        String[] scoresStr = bf.readLine().split(" ");
+
+        for (int i = 0; i < n; i++) {
+            scores[i] = Integer.parseInt(scoresStr[i]);
+        }
+
+        int max = 0;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (scores[i] > max) {
+                max = scores[i];
+            }
+            sum += scores[i];
+        }
+        // 소수점 표현을 위해 100.0으로 연산을 진행(실수형 데이터가 연산과정에 있어야 결과도 실수로 나옴)
+        // 나누기 전에 sum * 100.0을 먼저 해야 나누기 결과에서 몫이 안 나오고 실수형이 나옴
+        double result = (sum * 100.0 / max) / n;
+        System.out.println(result);
+    }
+}
